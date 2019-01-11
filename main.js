7f4499597af33a7c0a9dbe41960bf44df920e679
unknown <5t4mp5@gmail.com>
2019-01-11 16:36:41 -0500

    create hierarchy, and have text output

---
+++ main.js
@@ -0,0 +1,53 @@
+const users = [
+	{ id: 1, name: 'moe' },
+	{ id: 2, name: 'larry', managerId: 1},
+	{ id: 3, name: 'curly', managerId: 2 },
+	{ id: 4, name: 'shep', managerId: 1 },
+	{ id: 5, name: 'groucho', managerId: 4}
+];
+
+class EmployeeTree{
+    constructor(employee){
+        this.employee = employee;
+        this.name = employee.name;
+        this.subordinates = [];
+    }
+    
+    insert(employee){
+      if(employee.managerId === this.employee.id){
+        this.subordinates.push(new EmployeeTree(employee));
+      }else{
+        this.subordinates.forEach(sub =>{
+          sub.insert(employee);
+        });
+      }
+    }
+}
+
+function showManagementStructure(hArr){
+    const eTree = new EmployeeTree(hArr.shift());
+
+    hArr.forEach(employee => {
+        eTree.insert(employee);
+    });
+
+    return eTree
+}
+
+const myTree = showManagementStructure(users);
+
+
+function printTree(employee){
+  let output = '';
+
+  output += `{${employee.name}: `;
+  if(employee.subordinates){
+    output += employee.subordinates.map(sub => printTree(sub)) + '}';
+  }
+
+
+
+  return output;
+}
+
+printTree(myTree);
