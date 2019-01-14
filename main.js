e98f1b1b0392331663d763e12ea4657d985e7d4b
unknown <5t4mp5@gmail.com>
2019-01-14 14:32:18 -0500

    logs out string of hierarchy

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+/*I wasn't exaclty sure if the spec of this homework was to return a string representing the hierarchy, like the one shown in the ReadMe, or to return a data
+structure representing that hierarchy. I set up my function to console log out the text string, and return a data structure with each employee having 
+an array of their subordinates. 
+*/
 const users = [
   { id: 1, name: 'moe' },
   { id: 2, name: 'larry', managerId: 1 },
@@ -23,6 +27,19 @@ class EmployeeTree {
   }
 }
 
+function printTree(emp, padder = ''){
+  let output = '';
+  output += emp.employee.name + '\n';
+  if(emp.subordinates.length){
+    padder += '   '
+    emp.subordinates.forEach(sub => {
+      output += padder;
+      output += '--' + printTree(sub, padder);
+    });
+  }
+  return output;
+}
+
 function showManagementStructure(hArr) {
   const eTree = new EmployeeTree(hArr.shift());
 
@@ -30,6 +47,10 @@ function showManagementStructure(hArr) {
     eTree.insert(employee);
   });
 
+  console.log(printTree(eTree));
+
   return eTree;
 }
+
+
 
