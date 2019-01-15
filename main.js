075ca5d5c6f82b5ade42498e2f35ccd280fc1f78
unknown <5t4mp5@gmail.com>
2019-01-15 13:47:37 -0500

    fixed start with ref to boss

--- main.js
+++ main.js
@@ -1,68 +1,25 @@
 const users = [
-  { id: 1, name: 'moe' },
   { id: 2, name: 'larry', managerId: 1 },
   { id: 3, name: 'curly', managerId: 2 },
+  { id: 1, name: 'moe' },
   { id: 4, name: 'shep', managerId: 1 },
   { id: 5, name: 'groucho', managerId: 4 },
 ];
 
 
-function showManagementStructure(hArr, current, pad = '-'){
-  let arrCopy = hArr.slice();
+function showManagementStructure(hArr, current, pad = ''){
   if(!current){
-    current = arrCopy.shift();
+    current = hArr.filter(user => !user.managerId);
+    console.log(current.name);
+    pad += '-'
+  } else{
+    console.log(pad, current.name);
   }
 
-  console.log(pad, current.name);
-
-  const subordinates = arrCopy.filter(user => user.managerId === current.id);
+  const subordinates = hArr.filter(user => user.managerId === current.id);
 
   if(subordinates.length){
     pad = '   ' + pad;
     subordinates.forEach(sub => showManagementStructure(hArr, sub, pad));
   }
-
 }
-
-/*class EmployeeTree {
-  constructor(employee) {
-    this.employee = employee;
-    this.subordinates = [];
-  }
-
-  insert(employee) {
-    if (employee.managerId === this.employee.id) {
-      this.subordinates.push(new EmployeeTree(employee));
-    } else {
-      this.subordinates.forEach(sub => {
-        sub.insert(employee);
-      });
-    }
-  }
-  printTree(padder = '') {
-    let output = '';
-    output += this.employee.name + '\n';
-    if (this.subordinates.length) {
-      padder += '       ';
-      this.subordinates.forEach(sub => {
-        output += padder;
-        output += '-' + sub.printTree(padder);
-      });
-    }
-    return output;
-  }
-}
-
-function showManagementStructure(hArr) {
-  const eTree = new EmployeeTree(hArr.shift());
-
-  hArr.forEach(employee => {
-    eTree.insert(employee);
-  });
-
-  return eTree.printTree();
-}*/
-
-
-
-
